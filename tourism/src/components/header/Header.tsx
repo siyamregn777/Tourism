c2e594c4718e38ba490c56d3fd1c348c2dd6f098
siyamregn777 <siyamregnyeshidagna777@gmail.com>
2024-12-18 11:24:46 +0300

    update header

--- tourism/src/components/header/Header.tsx
+++ tourism/src/components/header/Header.tsx
@@ -1,11 +1,11 @@
-// import Navbar from '@/components/navbar/Navbar'; // Import Navbar
+import Navbar from '@/components/navbar/Navbar'; // Import Navbar
 import styles from './Header.module.css'; // Import CSS module
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Journey</h1>
-      {/* <Navbar /> Include Navbar here */}
+      <Navbar /> {/* Include Navbar here */}
     </header>
   );
 };
