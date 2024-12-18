4d94e25e2463743e9d88a85d6265eaee81683208
siyamregn777 <siyamregnyeshidagna777@gmail.com>
2024-12-18 15:16:32 +0300

    test connection tha backned

---
+++ tourism/src/app/api/tesconnection/route.js
@@ -0,0 +1,24 @@
+import clientPromise from '@/lib/mongodb';
+
+export async function GET() {
+  try {
+    const client = await clientPromise; // Connect to MongoDB
+    const db = client.db(); // Get the default database
+
+    return new Response(
+      JSON.stringify({
+        message: 'Successfully connected to MongoDB Atlas',
+        dbName: db.databaseName,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: 'Failed to connect to MongoDB Atlas',
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
